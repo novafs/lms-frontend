dbf418f67c93ee7f575a145dcd49ef169cafc808
Nova Fajri <novafajri20@gmail.com>
2025-10-30 21:57:05 +0700

    Suspense konfigurasi

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 function App() {
-  
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
