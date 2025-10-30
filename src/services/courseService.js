f25d9c4890f052f2c69ea1f5ff5cfe99f22ad9f1
Nova Fajri <novafajri20@gmail.com>
2025-10-30 22:37:40 +0700

    delete header Content Type for Axios Header Override

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -3,17 +3,9 @@ import { apiInstanceAuth } from "../utils/axios"
 
 export const getCourses = async () => apiInstanceAuth.get('/courses').then(res => res.data)
 export const getCategories = async () => apiInstanceAuth.get('/categories').then(res => res.data)
-export const createCourse = async (data) => apiInstanceAuth.post('/courses', data, {
-    headers: {
-        "Content-Type": "multipart/form-data",
-    },
-}).then((res) => res.data)
+export const createCourse = async (data) => apiInstanceAuth.post('/courses', data).then((res) => res.data)
 export const getCourseDetail = async (id, isPreview = false) => apiInstanceAuth.get(`/courses/${id}${isPreview ? '?preview=true' : ''}`).then(res => res.data)
-export const updateCourse = async (data, id) => apiInstanceAuth.put(`/courses/${id}`, data, {
-    headers: {
-        "Content-Type": "multipart/form-data",
-    },
-}).then((res) => res.data)
+export const updateCourse = async (data, id) => apiInstanceAuth.put(`/courses/${id}`, data).then((res) => res.data)
 export const deleteCourse = async(id) =>  apiInstanceAuth.delete(`/courses/${id}`).then((res) => res.data)
 
 export const createContent = async (data) => apiInstanceAuth.post('/courses/contents', data).then(res => res.data)
