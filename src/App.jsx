5d9b9f130036039230b5c7785691277cbf79f832
Nova Fajri <novafajri20@gmail.com>
2025-10-30 22:38:33 +0700

    import Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient();
 function App() {
